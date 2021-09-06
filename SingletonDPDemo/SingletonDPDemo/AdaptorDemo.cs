59872cb559a2896107bda1d2f0d86569731dafb9
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-09-06 15:14:56 +0530

    Push New Project

---
+++ SingletonDPDemo/SingletonDPDemo/AdaptorDemo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDPDemo
+{
+    class AdaptorDemo
+    {
+        static void Main()
+        {
+            Target target1 = new Target();
+            target1.Request();
+
+            Target target = new Adapter();
+            target.Request();
+            Console.ReadLine();
+        }
+    }
+
+    public class Target
+    {
+        public virtual void Request()
+        {
+            Console.WriteLine("Called Target Request()");
+        }
+    }
+    /// <summary>
+    /// The 'Adapter' class
+    /// </summary>
+    public class Adapter : Target
+    {
+        private Adaptee adaptee = new Adaptee();
+        public override void Request()
+        {
+            // Possibly do some other work
+            // and then call SpecificRequest
+            adaptee.SpecificRequest();
+        }
+    }
+    /// <summary>
+    /// The 'Adaptee' class
+    /// </summary>
+    public class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Called SpecificRequest()");
+        }
+    }
+}
---
+++ SingletonDPDemo/SingletonDPDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDPDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  The constructor is protected---can not use new
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+
+            //Test instances
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objects are the same instances");
+            }
+            Console.WriteLine(Singleton.Message("Hello"));
+            Console.WriteLine(Singleton.Message("Hello World"));
+            Console.ReadLine();
+        }
+    }
+   public class Singleton
+    {
+        static Singleton instance;
+        //  Constructor is protected
+        protected Singleton()
+        {
+            Console.WriteLine("Constructor invoked");
+        }
+
+        public static Singleton Instance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+               
+            }
+            return instance;
+        }
+    
+        public static string Message(string msg)
+        {
+            return msg;
+        }
+    }
+}
